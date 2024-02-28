92487509599fe89ca70a23d8cee2be3cdeab8e93
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-02-28 22:29:30 +0300

    completed

---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entitiy;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult MusteriListele()
+        {
+            var degerler = db.TBLMUSTERI.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ Controllers/UrunController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entitiy;
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities(); 
+        public ActionResult UrunList()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+    }
+}
